9aa709b3f558d9347d00db12a1663699fb96a890
4727yesuraju <94819056+4727yesuraju@users.noreply.github.com>
2024-12-10 07:10:14 +0530

    adding two linked lists

--- SingleLinkedList.java
+++ SingleLinkedList.java
@@ -22,7 +22,7 @@ public class SingleLinkedList{
 
     }
 
-    void display(){
+    static void display(ListNode head){
         ListNode curr = head;
         //System.out.print("head --> ");
         while(curr != null){
@@ -199,10 +199,10 @@ public class SingleLinkedList{
     }
     void createLoop(){
         ListNode first = new ListNode(1);
-        ListNode second = new ListNode(1);
-        ListNode third = new ListNode(1);
-        ListNode fourth = new ListNode(1);
-        ListNode fifth = new ListNode(1);
+        ListNode second = new ListNode(2);
+        ListNode third = new ListNode(3);
+        ListNode fourth = new ListNode(4);
+        ListNode fifth = new ListNode(5);
 
         head = first;
         first.next = second;
@@ -225,12 +225,124 @@ public class SingleLinkedList{
         }
         return false;
      }
+    ListNode containsLoopAndReturnFirst(){
+        ListNode slowPtr = head;
+        ListNode fastPtr = head;
+        while(fastPtr != null && fastPtr.next!=null){
+            fastPtr = fastPtr.next.next;
+            slowPtr = slowPtr.next;
+            if(fastPtr == slowPtr){
+                return returnFirst(slowPtr);
+            }
+        }
+        return null;
+     }
 
+     ListNode returnFirst(ListNode slowPtr){
+        ListNode temp = head;
+        while(temp!=slowPtr){
+            temp = temp.next;
+            slowPtr = slowPtr.next;
+        }
+        return slowPtr;
+     }
+
+    void removeLoop(){
+        ListNode slowPtr = head;
+        ListNode fastPtr = head;
+        while(fastPtr != null && fastPtr.next!=null){
+            fastPtr = fastPtr.next.next;
+            slowPtr = slowPtr.next;
+            if(fastPtr == slowPtr){
+                removeLoop(slowPtr);
+            }
+        }
+     }
+
+     void removeLoop(ListNode slowPtr){
+        ListNode temp = head;
+        while(slowPtr.next!=temp.next){
+            slowPtr = slowPtr.next;
+            temp = temp.next;
+        }
+        slowPtr.next = null;
+     }
+
+     static ListNode merge(ListNode a, ListNode b){
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+        while(a!=null && b!=null){
+            if(a.data <= b.data){
+                tail.next = a;
+                a = a.next;
+            }else{
+                tail.next = b;
+                b = b.next;
+            }
+            tail = tail.next;
+        }
+        if(a!=null) tail.next = a;
+        else tail.next = b;
+        return dummy.next;
+     }
+
+     static ListNode add(ListNode a, ListNode b){
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+        int carry = 0;
+        while(a!= null || b!=null){
+            int x = a!=null ? a.data : 0;
+            int y = b!=null ? b.data : 0;
+            int sum = x+y+carry;
+            carry = sum/10;
+            tail.next = new ListNode(sum%10);
+            tail = tail.next;
+            if(a!=null) a= a.next;
+            if(b!=null) b=b.next;
+        }
+
+        if(carry >0)  tail.next = new ListNode(carry);
+
+        return dummy.next;
+     }
     public static void main(String args[]){
-         SingleLinkedList sll = new SingleLinkedList();
-         sll.createLoop();
-         if(sll.containsLoop()) System.out.println("contains loop");
-         else System.out.println("no loop");
+         SingleLinkedList sll1 = new SingleLinkedList();
+         SingleLinkedList sll2 = new SingleLinkedList();
+
+         sll1.insertEnd(7);
+         sll1.insertEnd(4);
+         sll1.insertEnd(9);
+         sll2.insertEnd(5);
+          sll2.insertEnd(6);
+        //  sll2.insertEnd(2);
+        //  sll2.insertEnd(3);
+
+         ListNode temp = add(sll1.head,sll2.head);
+         display(temp);
+        //  sll1.insertEnd(1);
+        //  sll1.insertEnd(3);
+        //  sll1.insertEnd(5);
+        //  sll1.insertEnd(7);
+        //  sll2.insertEnd(2);
+        //  sll2.insertEnd(4);
+        //  sll2.insertEnd(6);
+        //  sll2.insertEnd(8);
+        //  sll2.insertEnd(10);
+        //  sll2.insertEnd(12);
+        //  display(sll1.head);
+        //  display(sll2.head);
+        // ListNode temp = merge(sll1.head, sll2.head);
+        // display(temp);
+        //sll.display();
+        //  sll.createLoop();
+        //  if(sll.containsLoop()) System.out.println("contains loop");
+        //  else System.out.println("no loop");
+
+        //  System.out.println(sll.containsLoopAndReturnFirst().data);
+        //  sll.removeLoop();
+        // if(sll.containsLoop()) System.out.println("contains loop");
+        // else System.out.println("no loop");
+
         // sll.insertEnd(1);
         // sll.insertEnd(2);
         // sll.insertEnd(2);
