fd38990dfa321f8da68dbbc20643930905d91f4b
4727yesuraju <94819056+4727yesuraju@users.noreply.github.com>
2025-03-18 07:14:11 +0530

    circular single linkedlist is done

--- circularLinkedList/index.js
+++ circularLinkedList/index.js
@@ -32,7 +32,7 @@ class circulaLinkedList{
     }
 
     display(){
-        if(!last) return console.log('list is Empty')
+        if(!this.last) return console.log('list is Empty')
         let temp = this.last.next;
         let str = "";
         while(temp !== this.last){
@@ -42,8 +42,56 @@ class circulaLinkedList{
         str += `${temp.data} --> null`
         console.log(str);
     }
+
+    insertFirst(data){
+        let newNode = new Node(data);
+        if(!this.last) this.last = newNode;
+        else newNode.next = this.last.next;
+        this.last.next = newNode
+        return ++this.length;
+    }
+
+    insertLast(data){
+        let newNode = new Node(data);
+        if(!this.last) {
+            this.last = newNode;
+            this.last.next = newNode;
+        }else{
+            newNode.next = this.last.next;
+            this.last.next = newNode;
+            this.last = newNode
+        }
+        return ++this.length;
+    }
+
+    deleteFirst(){
+        if(this.isEmpty()) return console.log("List is Empty");
+        let temp = this.last.next;
+        if(this.last.next === this.last) this.last = null;
+        else this.last.next = temp.next;
+        temp.next = null;
+        --this.length;
+        return temp;
+        
+    }
 }
 
 const cll = new circulaLinkedList();
-cll.createCircularLinkedList();
+// cll.createCircularLinkedList();
+
+cll.insertFirst(1);
+cll.insertFirst(2);
+cll.insertFirst(3);
+cll.insertFirst(4);
+
+cll.insertLast(5);
+cll.insertLast(6);
+cll.insertLast(7);
+cll.insertLast(8);
+
+
+console.log(cll.deleteFirst());
+cll.deleteFirst();
+cll.deleteFirst();
+cll.deleteFirst();
 cll.display();
