034ac7170e39bd2d5e2190fc516eae6d1ce977f8
4727yesuraju <94819056+4727yesuraju@users.noreply.github.com>
2025-04-09 07:06:35 +0530

    containe loop

--- LinkedList.java
+++ LinkedList.java
@@ -124,6 +124,79 @@ public class LinkedList{
         return prev;
     }
 
+    Node nthNodeFromLast(int n){
+        Node mainPtr = head;
+        Node refPtr = head;
+        int count = 0;
+        while(count < n){
+            refPtr = refPtr.next;
+            count++;
+        }
+        while(refPtr!=null){
+            refPtr = refPtr.next;
+            mainPtr = mainPtr.next;
+        }
+        return mainPtr;
+    }
+
+    void removeDuplicatesFromSortedList(){
+        Node curr = head;
+        while(curr!=null && curr.next!=null){
+            if(curr.data == curr.next.data) curr.next = curr.next.next;
+            else curr = curr.next;
+        }
+    }
+
+    void insertInsortedList(int data){
+        Node prev = null;
+        Node curr = head;
+        while(curr!=null && curr.data < data){
+           prev = curr;
+           curr = curr.next;
+        }
+        Node newNode = new Node(data);
+        newNode.next = curr;
+        prev.next = newNode;
+    }
+
+    void removeKey(int key){
+        Node prev = null;
+        Node curr = head;
+        while(curr!=null && curr.data != key){
+            prev = curr;
+            curr = curr.next;
+        }
+        if(curr == null) return;
+        prev.next = curr.next;
+    }
+
+    void createLoop(){
+        Node first = new Node(1);
+        Node sec = new Node(2);
+        Node third = new Node(3);
+        Node fourth = new Node(4);
+        Node fifth = new Node(5);
+
+        head = first;
+        first.next = sec;
+        sec.next = third;
+        third.next = fourth;
+        fourth.next = fifth;
+        fifth.next = sec;
+    }
+
+    boolean containesLoop(){
+        Node fastPtr = head;
+        Node slowPtr = head;
+        while(fastPtr != null && fastPtr.next != null){
+            fastPtr = fastPtr.next.next;
+            slowPtr = slowPtr.next;
+            if(fastPtr == slowPtr) return true;
+        }
+        return false;
+    }
+
+
     public static void main(String args[]){
         LinkedList ll = new LinkedList();
         // ll.head = new Node(1);
@@ -138,21 +211,34 @@ public class LinkedList{
 
         // System.out.println(ll.length());
 
-        ll.insertFirst(4);
-        ll.insertFirst(2);
-        ll.insertFirst(1);
+        // ll.insertFirst(4);
+        // ll.insertFirst(4);
+        // ll.insertFirst(2);
+        // ll.insertFirst(1);
 
-        ll.insertAt(3, 3);
+        // ll.insertAt(3, 3);
         // ll.print(ll.head);
         // ll.deleteFirst();
         // ll.print(ll.head);
         // ll.deleteLast();
         // ll.print(ll.head);
         // ll.deleteAt(2);
-        ll.print(ll.head);
-        System.out.println(ll.search(30));
+        // ll.print(ll.head);
+        // System.out.println(ll.search(30));
         // ll.reverse();
-        ll.print(ll.reverse());
+        // ll.print(ll.reverse());
+        // System.out.println(ll.nthNodeFromLast(2).data);
+        // ll.removeDuplicatesFromSortedList();
+        // ll.print(ll.head);
+        // ll.insertInsortedList(30);
+        // ll.print(ll.head);
+
+        // ll.removeKey(30);
+        // ll.print(ll.head);
+
+        ll.createLoop();
+        // ll.print(ll.head);
+        System.out.println(ll.containesLoop());
 
 
     }
