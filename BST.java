4a4b0ce668f19019283b9fdd224396f1f06c73aa
4727yesuraju <94819056+4727yesuraju@users.noreply.github.com>
2025-04-23 07:07:06 +0530

    bst is done

--- BST.java
+++ BST.java
@@ -20,7 +20,7 @@ class BST{
             root = new Node(data);
             return root;
         }
-        if(root.data<data){
+        if(data<root.data){
             root.left = insert(root.left,data);
         }else{
             root.right = insert(root.right,data);
@@ -35,6 +35,23 @@ class BST{
         inOrder(root.right);
     }
 
+    Node search(Node root,int key){
+        if(root == null || key == root.data) return root;
+        if(key<root.data) return search(root.left,key);
+        else return search(root.right,key);
+    }
+
+    boolean isValid(Node root, int min, int max){
+        if(root == null) return true;
+        if(root.data <= min || root.data >= max) return false;
+        boolean left = isValid(root.left,min,root.data);
+        if(left){
+            boolean right = isValid(root.right, root.data, max);
+            return right;
+        }
+        return false;
+    }
+
     public static void main(String args[]){
            BST bst = new BST();
            bst.insert(1);
@@ -42,8 +59,12 @@ class BST{
            bst.insert(4);
            bst.insert(5);
            bst.insert(2);
-
-
            bst.inOrder(bst.root);
+           System.out.println();
+           Node res = bst.search(bst.root,20);
+           if(res != null){
+            System.out.println(res.data);
+           }
+           System.out.println(bst.isValid(bst.root, Integer.MIN_VALUE, Integer.MAX_VALUE));
     }
 }
